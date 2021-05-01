f70a85c16a43de801777d219657c9ca453b7f39b
ashraf <ashrafeid@outlook.com>
2021-05-01 15:14:50 +0300

    toggle submenu to open the subsequent list

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,5 +2,15 @@ document.querySelector('.toggle-sidebar').onclick = function(){
     document.querySelector('.sidebar').classList.toggle('no-sidebar')
     document.querySelector('.content-area').classList.toggle('no-sidebar')
 }
+
+let submenu = document.querySelectorAll('.toggle-submenu')
+let childLinks = document.querySelectorAll('.child-links')
+for (let i = 0 ; i < submenu.length; i++){
+    submenu[i].onclick = function(){
+        submenu[i].classList.toggle('down')
+        childLinks[i].classList.toggle('show')
+    }
+
+}
 const myMap = new Map()
 const mySet = new Set()
--- stage/js/main.js
+++ stage/js/main.js
@@ -2,3 +2,13 @@ document.querySelector('.toggle-sidebar').onclick = function(){
     document.querySelector('.sidebar').classList.toggle('no-sidebar')
     document.querySelector('.content-area').classList.toggle('no-sidebar')
 }
+
+let submenu = document.querySelectorAll('.toggle-submenu')
+let childLinks = document.querySelectorAll('.child-links')
+for (let i = 0 ; i < submenu.length; i++){
+    submenu[i].onclick = function(){
+        submenu[i].classList.toggle('down')
+        childLinks[i].classList.toggle('show')
+    }
+
+}
